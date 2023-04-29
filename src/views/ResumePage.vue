47bc048533eed87ed5a7df480eed16d1032bc26b
KiAi <94138724+broccoli88@users.noreply.github.com>
2023-04-29 12:13:07 +0200

    AboutMe text fix + fixed icons

--- src/views/ResumePage.vue
+++ src/views/ResumePage.vue
@@ -1,96 +1,96 @@
 <template>
-    <article class="resume-page">
-        <h1>Resume</h1>
-        <h2>Pawel Jaromin</h2>
-        <div class="buttons">
-            <Button @click="ETGoHome">Home</Button>
-            <a
-                class="download"
-                href="https://mega.nz/file/vJ8BGS6Q#FsfdYbL30H2Hr4Ovf3DHlQKTyh0JkQ_w8Cd83Zvtb2w"
-                download
-                target="_blank"
-            >
-                <Button>Download</Button>
-            </a>
-        </div>
-        <figure class="resume-scan">
-            <img src="../assets/Resume_Pawel_Jaromin.png" alt="" />
-        </figure>
-    </article>
+	<article class="resume-page">
+		<h1>Resume</h1>
+		<h2>Pawel Jaromin</h2>
+		<div class="buttons">
+			<Button @click="ETGoHome">Home</Button>
+			<a
+				class="download"
+				href="https://mega.nz/file/GZsXhS7I#QogCYAmfIlx0yDsQiAoiac-OpGvxcNwBnowmcIfVqV8"
+				download
+				target="_blank"
+			>
+				<Button>Download</Button>
+			</a>
+		</div>
+		<figure class="resume-scan">
+			<img src="../assets/Pawel_Jaromin_Resume_EN.png" alt="" />
+		</figure>
+	</article>
 
-    <Footer />
+	<Footer />
 </template>
 
 <script>
-import Button from "../components/Button.vue";
-import Footer from "../components/Footer.vue";
+	import Button from "../components/Button.vue";
+	import Footer from "../components/Footer.vue";
 
-const scrollAnimation = {
-    mounted: (el) => {
-        let option = {
-            root: null,
-            rootMargin: "-150px",
-            threshold: 0,
-        };
+	const scrollAnimation = {
+		mounted: (el) => {
+			let option = {
+				root: null,
+				rootMargin: "-150px",
+				threshold: 0,
+			};
 
-        const animationObserver = new IntersectionObserver(
-            (entries, animationObserver) => {
-                entries.forEach((entry) => {
-                    if (!entry.isIntersecting) return;
-                    el.classList.toggle("on-entry");
-                    animationObserver.unobserve(el);
-                });
-            }
-        );
-        animationObserver.observe(el);
-    },
-};
+			const animationObserver = new IntersectionObserver(
+				(entries, animationObserver) => {
+					entries.forEach((entry) => {
+						if (!entry.isIntersecting) return;
+						el.classList.toggle("on-entry");
+						animationObserver.unobserve(el);
+					});
+				}
+			);
+			animationObserver.observe(el);
+		},
+	};
 
-export default {
-    directives: { scrollAnimation },
-    components: { Button, Footer },
+	export default {
+		directives: { scrollAnimation },
+		components: { Button, Footer },
 
-    methods: {
-        ETGoHome() {
-            this.$router.push("/");
-        },
-    },
-};
+		methods: {
+			ETGoHome() {
+				this.$router.push("/");
+			},
+		},
+	};
 </script>
 
 <style>
-.resume-page {
-    margin: 0 auto;
-    max-width: min(100%, 120rem);
-    min-height: 100vh;
-    padding: clamp(5rem, 7vw, 8.5rem);
+	.resume-page {
+		margin: 0 auto;
+		max-width: min(100%, 120rem);
+		min-height: 100vh;
+		padding: clamp(5rem, 7vw, 8.5rem);
 
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-}
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+	}
 
-.resume-page > h2 {
-    color: var(--color-secondary);
-    margin-bottom: 4rem;
-}
-.resume-scan {
-    margin-top: 5rem;
-    width: 100%;
-    height: 100%;
-}
+	.resume-page > h2 {
+		color: var(--color-secondary);
+		margin-bottom: 4rem;
+	}
+	.resume-scan {
+		margin-top: 5rem;
+		width: 100%;
+		height: 100%;
+	}
 
-.resume-scan > img {
-    width: 100%;
-    object-fit: cover;
-}
+	.resume-scan > img {
+		width: 100%;
+		object-fit: cover;
+	}
 
-.buttons {
-    display: flex;
-}
+	.buttons {
+		display: flex;
+	}
 
-.download {
-    text-decoration: none;
-    color: inherit;
-}
+	.download {
+		text-decoration: none;
+		color: inherit;
+	}
 </style>
